f50cb066551fd5ed971ab3f0ea9168f294219b15
Jatin <jatin@Jatins-MacBook-Air.local>
2019-10-12 00:14:18 +0530

    first commit

---
+++ app/src/main/java/com/example/notesapp/NoteEditorActivity.java
@@ -0,0 +1,28 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        EditText editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+
+        int noteId = intent.getIntExtra("noteId",-1);
+
+        if(noteId!=-1){
+
+            editText.setText(MainActivity.notes.get(noteId));
+
+        }
+    }
+}
